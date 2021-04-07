43736a98d4911f2221c92fc7103e5008dc0dd99a
LOWTASTE <544851589@QQ.COM>
2021-04-07 13:34:20 +0800

    version_1.2

--- app/src/main/java/com/example/musicplayer/DisplayActivity.java
+++ app/src/main/java/com/example/musicplayer/DisplayActivity.java
@@ -3,6 +3,7 @@ import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -24,19 +25,19 @@ public class DisplayActivity extends AppCompatActivity {
         //获取传入信息
         int time = getIntent().getIntExtra("time",0);
         int currentPosition = getIntent().getIntExtra("currentPosition", -1);
-        int position = getIntent().getIntExtra("Position", -1);
+        int position = getIntent().getIntExtra("position", -1);
 //        String[] nameStr =  getIntent().getStringArrayExtra("nameStr");
         int[] idStr = getIntent().getIntArrayExtra("idStr");
         boolean playMode = getIntent().getBooleanExtra("playMode", false);
 
         //播放逻辑
-//        if((position != currentPosition) || !playMode){
-//            music.reset();
-//            music = MediaPlayer.create(context,idStr[position]);
-//            music.start();
-//        }
 
-
+        if((position != currentPosition) || !playMode){
+            Log.d("IN", "进入播放分支");
+            music.reset();
+            music = MediaPlayer.create(context,idStr[position]);
+            music.start();
+        }
 
         Integer Time = time/1000;
         time_info.setText(Time.toString());
